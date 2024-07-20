b697cb1d91d788ae6b98d0e8a59cac5fd8099834
zeeshan <7up.charsi@gmail.com>
2024-07-20 14:38:44 +0500

    feat: design tokens

--- skillset-e2e/ecobazar/tailwind.config.ts
+++ skillset-e2e/ecobazar/tailwind.config.ts
@@ -1,20 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontSize: {
+        display1: '72px',
+        h1: '56px',
+        h2: '48px',
+        h3: '40px',
+        h4: '36px',
+        h5: '32px',
       },
     },
   },
   plugins: [],
 };
 export default config;
+
