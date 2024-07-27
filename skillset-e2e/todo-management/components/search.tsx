dbbada3b22671923bc7f327a1ebbaff7f7e289a4
zeeshan <7up.charsi@gmail.com>
2024-07-27 22:29:35 +0500

    feat: seach

--- skillset-e2e/todo-management/components/search.tsx
+++ skillset-e2e/todo-management/components/search.tsx
@@ -1,4 +1,11 @@
+'use client';
+
+import { useRouter, useSearchParams } from 'next/navigation';
+import { priorities, statuses } from '@/constants/common';
 import { Input } from '@typeweave/react/input';
+import { useStore } from '@/zustand/store';
+import debounce from 'lodash.debounce';
+import Fuse from 'fuse.js';
 import React from 'react';
 
 interface SearchProps {}
@@ -8,11 +15,38 @@ const displayName = 'Search';
 export const Search = (props: SearchProps) => {
   const {} = props;
 
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [value, setvalue] = React.useState(
+    searchParams.get('query') ?? '',
+  );
+
+  const [fuseInstance, setFuseInstance] = React.useState<Fuse<{
+    title: string;
+  }> | null>(null);
+
+  const debounced = React.useMemo(
+    () =>
+      debounce((value: string) => {
+        const searchParams = new URLSearchParams();
+
+        searchParams.set('query', value);
+
+        router.push(`/?${searchParams.toString()}`);
+      }, 500),
+    [router],
+  );
+
   return (
     <search className="grow">
       <Input
         label="search tasks"
         hideLabel
+        value={value}
+        onChange={(e) => {
+          setvalue(e.target.value);
+          debounced(e.target.value);
+        }}
         placeholder="Search Tasks..."
         className="w-full"
         classNames={{ inputWrapper: 'h-9' }}
