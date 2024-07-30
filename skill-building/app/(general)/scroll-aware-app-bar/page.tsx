b22b9a4c91b46586a5c6c857a32fdf33de90920d
zeeshan <7up.charsi@gmail.com>
2024-07-30 20:46:01 +0500

    refactor: paragraph designs

--- skill-building/app/(general)/scroll-aware-app-bar/page.tsx
+++ skill-building/app/(general)/scroll-aware-app-bar/page.tsx
@@ -4,13 +4,27 @@ export default function Home() {
   return (
     <>
       <AppBar />
-      <main className="mt-16 space-y-3 p-5">
-        {Array.from({ length: 150 }).map((_, i) => (
+      <main className="mt-16 p-5">
+        {Array.from({ length: 10 }).map((_, i) => (
           <div
             key={i}
-            style={{ width: `${Math.random() * 50 + 50}%` }}
-            className="h-3 rounded bg-gray-200"
-          ></div>
+            className="my-5 space-y-3 first:mt-0 last:mb-0"
+          >
+            {Array.from({
+              length: Math.floor(Math.random() * (15 - 4 + 1)) + 4,
+            }).map((_, i, arr) => (
+              <div
+                key={i}
+                style={{
+                  width:
+                    i + 1 === arr.length
+                      ? `${Math.floor(Math.random() * (70 - 10 + 1)) + 10}%`
+                      : '100%',
+                }}
+                className="h-3 rounded bg-gray-200"
+              ></div>
+            ))}
+          </div>
         ))}
       </main>
     </>
