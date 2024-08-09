35ca36547e69f7389f41a292289d7fcf580e9722
zeeshan <7up.charsi@gmail.com>
2024-08-10 03:40:35 +0500

    chore: swipeable side bar incomplete project

---
+++ incomplete-projects/swipeable-side-bar/layout.tsx
@@ -0,0 +1,23 @@
+import { SideBar } from './__components/side-bar';
+import { createMetadata } from '@/utils/metadata';
+import { AppBar } from './__components/app-bar';
+import { siteConfig } from './site.config';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = createMetadata(siteConfig);
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <AppBar />
+
+      <SideBar />
+
+      {children}
+    </>
+  );
+}
---
+++ incomplete-projects/swipeable-side-bar/site.config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  pathname: '/swipeable-side-bar',
+  name: 'swipeable side bar',
+  description:
+    'Swipeable Side Bar - Developed to demonstrate my expertise in web front-end development, featuring a responsive, expandable side bar that toggles on button press or swipe-to-right gesture, and hides on swipe-to-left motion',
+};
