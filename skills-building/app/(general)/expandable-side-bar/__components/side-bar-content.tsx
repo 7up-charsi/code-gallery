969772504735a879094efd6c9400ef1b1ff70002
zeeshan <7up.charsi@gmail.com>
2024-08-09 13:44:41 +0500

    feat(skills-building): expandable side bar complete

--- skills-building/app/(general)/expandable-side-bar/__components/side-bar-content.tsx
+++ skills-building/app/(general)/expandable-side-bar/__components/side-bar-content.tsx
@@ -1,5 +1,12 @@
+import {
+  Building2Icon,
+  CalendarIcon,
+  HomeIcon,
+  ListTodoIcon,
+  UserCogIcon,
+  UsersIcon,
+} from 'lucide-react';
 import { siteConfig } from '../site.config';
-import { HomeIcon } from 'lucide-react';
 import { NavLink } from './nav-link';
 import React from 'react';
 
@@ -15,29 +22,29 @@ export const SideBarContent = (props: SideBarContentProps) => {
   return (
     <nav
       data-expanded={isExpanded}
-      className="grid grid-cols-[48px] gap-3 px-5 data-[expanded=true]:grid-cols-[48px_1fr] md:px-3 md:py-3"
+      className="flex flex-col gap-3 px-5 md:px-3 md:py-3"
     >
       {[
         { label: 'home', href: '/', icon: <HomeIcon /> },
-        { label: 'page 1', href: '/page1', icon: <HomeIcon /> },
-        { label: 'page 2', href: '/page2', icon: <HomeIcon /> },
-        { label: 'page 3', href: '/page3', icon: <HomeIcon /> },
-        { label: 'page 4', href: '/page4', icon: <HomeIcon /> },
-        { label: 'page 5', href: '/page5', icon: <HomeIcon /> },
+        { label: 'users', href: '/page1', icon: <UsersIcon /> },
+        {
+          label: 'organizations',
+          href: '/page4',
+          icon: <Building2Icon />,
+        },
+        { label: 'calendar', href: '/page2', icon: <CalendarIcon /> },
+        { label: 'todo', href: '/page3', icon: <ListTodoIcon /> },
+
+        { label: 'settings', href: '/page5', icon: <UserCogIcon /> },
       ].map(({ icon, label, href }, i) => (
         <NavLink
           key={i}
           href={`${siteConfig.pathname}${href}`}
           data-expanded={isExpanded}
-          className="grid h-12 w-full grid-cols-subgrid items-center whitespace-nowrap rounded border border-transparent capitalize outline-none ring-focus hover:bg-muted-3 focus-visible:ring-2 active:bg-muted-4 data-[expanded=true]:col-span-2 data-[active=true]:border-primary-8 data-[active=true]:bg-primary-3 data-[active=true]:text-primary-11"
+          className="grid h-12 w-full grid-flow-col grid-cols-[46px] items-center overflow-hidden whitespace-nowrap rounded border border-transparent capitalize outline-none ring-focus hover:bg-muted-3 focus-visible:ring-2 active:bg-muted-4 data-[expanded=true]:grid-cols-[46px_1fr] data-[active=true]:border-primary-8 data-[active=true]:bg-primary-3 data-[active=true]:text-primary-11"
         >
-          <span
-            data-expanded={isExpanded}
-            className="mx-auto border-transparent data-[expanded=true]:border-r"
-          >
-            {icon}
-          </span>
-          {isExpanded && <span>{label}</span>}
+          <span className="mx-auto">{icon}</span>
+          <span>{label}</span>
         </NavLink>
       ))}
     </nav>
--- skills-building/app/(general)/expandable-side-bar/__components/side-bar.tsx
+++ skills-building/app/(general)/expandable-side-bar/__components/side-bar.tsx
@@ -17,28 +17,23 @@ export const SideBar = (props: SideBarProps) => {
   return (
     <aside
       data-expanded={isExpanded}
-      className="w-[72px] data-[expanded=true]:w-[230px]"
+      className="sticky left-0 top-[104px] h-[calc(100vh-104px)] w-[73px] border-r border-muted-6 transition-[width] data-[expanded=true]:w-[230px]"
     >
-      <div
-        data-expanded={isExpanded}
-        className="fixed left-0 top-[104px] h-[calc(100vh-108px)] border-r border-muted-6 data-[expanded=true]:w-[230px]"
+      <Button
+        isIconOnly
+        aria-label="shrink side bar"
+        size="sm"
+        color="primary"
+        variant="solid"
+        onPress={() => {
+          setIsExpanded((prev) => !prev);
+        }}
+        className="absolute right-0 top-1/2 size-5 -translate-y-1/2 translate-x-1/2 rounded-full"
       >
-        <Button
-          isIconOnly
-          aria-label="shrink side bar"
-          size="sm"
-          color="primary"
-          variant="solid"
-          onPress={() => {
-            setIsExpanded((prev) => !prev);
-          }}
-          className="absolute right-0 top-1/2 size-5 -translate-y-1/2 translate-x-1/2 rounded-full"
-        >
-          {isExpanded ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-        </Button>
+        {isExpanded ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+      </Button>
 
-        <SideBarContent isExpanded={isExpanded} />
-      </div>
+      <SideBarContent isExpanded={isExpanded} />
     </aside>
   );
 };
