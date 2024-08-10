71dc0ca443bba59ac8dedf8b0add1dffd1b587ba
zeeshan <7up.charsi@gmail.com>
2024-08-11 02:55:19 +0500

    refactor: swipeable side bar

--- skills-building/app/(general)/swipeable-side-bar/__components/app-bar.tsx
+++ skills-building/app/(general)/swipeable-side-bar/__components/app-bar.tsx
@@ -1,20 +1,16 @@
 'use client';
 
-import {
-  DrawerClose,
-  DrawerContent,
-  DrawerOverlay,
-  DrawerPortal,
-  DrawerRoot,
-  DrawerTrigger,
-} from '@typeweave/react/drawer';
+import { motion, useMotionValue, useTransform } from 'framer-motion';
+import { PointerEvents } from '@typeweave/react/pointer-events';
 import { PortfolioHeader } from '@/components/portfolio-header';
-import { useSwipeable } from '@typeweave/react/use-swipeable';
+import { useIsMounted } from '@typeweave/react/use-is-mounted';
 import { navLinks } from '../__constants/nav-links';
+import { Overlay } from '@typeweave/react/overlay';
 import { Button } from '@typeweave/react/button';
 import { Branding } from '@/components/branding';
 import { MenuIcon, XIcon } from 'lucide-react';
 import { siteConfig } from '../site.config';
+import { createPortal } from 'react-dom';
 import { NavLink } from './nav-link';
 import React from 'react';
 
@@ -25,49 +21,17 @@ const displayName = 'AppBar';
 export const AppBar = (props: AppBarProps) => {
   const {} = props;
 
+  const isPanningStartRef = React.useRef(false);
+  const panningStartPointRef = React.useRef(0);
+  const panningStartTimeRef = React.useRef(0);
+
+  const isMounted = useIsMounted();
+
   const [isScrolled, setIsScrolled] = React.useState(false);
-  const [isOpen, setIsOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-  const handlers = useSwipeable({
-    minDistance: 100,
-    onSwipeRight: ({ event }) => {
-      if (event.pointerType === 'touch') {
-        event.preventDefault();
-        setIsOpen(true);
-      }
-    },
-  });
-
-  React.useEffect(() => {
-    document.addEventListener('pointerdown', handlers.onPointerDown);
-    document.addEventListener('pointermove', handlers.onPointerMove);
-    document.addEventListener('pointerup', handlers.onPointerUp);
-    document.addEventListener(
-      'pointercancel',
-      handlers.onPointerCancel,
-    );
-
-    return () => {
-      document.removeEventListener(
-        'pointerdown',
-        handlers.onPointerDown,
-      );
-      document.removeEventListener(
-        'pointermove',
-        handlers.onPointerMove,
-      );
-      document.removeEventListener('pointerup', handlers.onPointerUp);
-      document.removeEventListener(
-        'pointercancel',
-        handlers.onPointerCancel,
-      );
-    };
-  }, [
-    handlers.onPointerCancel,
-    handlers.onPointerDown,
-    handlers.onPointerMove,
-    handlers.onPointerUp,
-  ]);
+  const x = useMotionValue(0);
+  const overlayOpacity = useTransform(x, [-280, 0], [0, 1]);
 
   React.useEffect(() => {
     const handleScroll = () => {
@@ -90,6 +54,65 @@ export const AppBar = (props: AppBarProps) => {
     };
   }, []);
 
+  React.useEffect(() => {
+    const handlePointerDown = (e: PointerEvent) => {
+      if (e.clientX < 100) {
+        e.preventDefault();
+        panningStartPointRef.current = e.clientX;
+        isPanningStartRef.current = true;
+        panningStartTimeRef.current = e.timeStamp;
+      }
+    };
+
+    const handlePointerUp = (e: PointerEvent) => {
+      isPanningStartRef.current = false;
+      panningStartPointRef.current = 0;
+
+      const start = panningStartPointRef.current;
+      const time = e.timeStamp - panningStartTimeRef.current;
+
+      const delta = e.clientX - start;
+
+      const isSwipe = delta >= 120 && time < 1000;
+
+      const currX = x.get();
+
+      if (isSwipe) {
+        x.set(0);
+        return;
+      }
+
+      if (currX < -140) {
+        x.set(-280);
+        return;
+      } else {
+        x.set(0);
+      }
+    };
+
+    const handlePointerMove = (e: PointerEvent) => {
+      if (isPanningStartRef.current && panningStartPointRef.current) {
+        const start = panningStartPointRef.current;
+
+        const isValidPanning = e.clientX > start;
+
+        if (!isValidPanning) return;
+
+        x.set(Math.min(e.clientX - start - 280, 0));
+      }
+    };
+
+    document.addEventListener('pointerdown', handlePointerDown);
+    document.addEventListener('pointerup', handlePointerUp);
+    document.addEventListener('pointermove', handlePointerMove);
+
+    return () => {
+      document.removeEventListener('pointerdown', handlePointerDown);
+      document.removeEventListener('pointerup', handlePointerUp);
+      document.removeEventListener('pointermove', handlePointerMove);
+    };
+  }, [x]);
+
   return (
     <header
       data-scrolled={isScrolled}
@@ -104,56 +127,17 @@ export const AppBar = (props: AppBarProps) => {
 
         <div className="grow"></div>
 
-        <DrawerRoot open={isOpen} onOpenChange={setIsOpen}>
-          <DrawerTrigger>
-            <Button
-              isIconOnly
-              aria-label="menu"
-              variant="text"
-              className="text-2xl lg:hidden"
-            >
-              <MenuIcon />
-            </Button>
-          </DrawerTrigger>
-
-          <DrawerPortal>
-            <DrawerOverlay className="lg:hidden" />
-            <DrawerContent className="lg:hidden">
-              <div className="flex h-16 items-center justify-between px-5">
-                <Branding href={siteConfig.pathname}>
-                  {siteConfig.name}
-                </Branding>
-
-                <DrawerClose>
-                  <Button
-                    isIconOnly
-                    aria-label="menu"
-                    variant="text"
-                    color="danger"
-                  >
-                    <XIcon />
-                  </Button>
-                </DrawerClose>
-              </div>
-
-              <nav
-                id="side-bar-nav"
-                className="flex flex-col gap-3 px-5 md:px-3 md:py-3"
-              >
-                {navLinks.map(({ icon, label, href }, i) => (
-                  <NavLink
-                    key={i}
-                    href={`${siteConfig.pathname}${href}`}
-                    className="grid h-12 w-full grid-cols-[46px_1fr] items-center overflow-hidden whitespace-nowrap rounded border border-transparent capitalize outline-none ring-focus hover:bg-muted-3 focus-visible:ring-2 active:bg-muted-4 data-[active=true]:border-primary-8 data-[active=true]:bg-primary-3 data-[active=true]:text-primary-11"
-                  >
-                    <span className="mx-auto">{icon}</span>
-                    <span>{label}</span>
-                  </NavLink>
-                ))}
-              </nav>
-            </DrawerContent>
-          </DrawerPortal>
-        </DrawerRoot>
+        <Button
+          isIconOnly
+          aria-label="menu"
+          variant="text"
+          className="text-2xl lg:hidden"
+          onPress={() => {
+            setOpen(true);
+          }}
+        >
+          <MenuIcon />
+        </Button>
 
         <nav className="flex items-center gap-1 max-lg:hidden">
           {navLinks.map((ele, i) => (
@@ -167,6 +151,69 @@ export const AppBar = (props: AppBarProps) => {
           ))}
         </nav>
       </div>
+
+      {isMounted &&
+        createPortal(
+          <>
+            <PointerEvents
+              onPress={(e) => {
+                if (e.currentTarget === e.target) {
+                  setOpen(false);
+                }
+              }}
+            >
+              <motion.div
+                data-open={open}
+                style={{ opacity: overlayOpacity }}
+                initial={{ opacity: 0 }}
+                className="fixed inset-0 z-40 bg-overlay lg:hidden"
+              />
+            </PointerEvents>
+
+            <motion.div
+              initial={{ x: -280 }}
+              style={{ x }}
+              className="fixed left-0 top-0 z-50 h-screen w-[280px] bg-white data-[open=false]:hidden lg:hidden"
+            >
+              <div className="flex h-16 items-center justify-between px-5">
+                <Branding href={siteConfig.pathname}>
+                  {siteConfig.name}
+                </Branding>
+
+                <Button
+                  isIconOnly
+                  aria-label="menu"
+                  variant="text"
+                  color="danger"
+                  onPress={() => {
+                    setOpen(false);
+                  }}
+                >
+                  <XIcon />
+                </Button>
+              </div>
+
+              <nav
+                id="side-bar-nav"
+                className="flex flex-col gap-3 px-5 md:px-3 md:py-3"
+              >
+                {navLinks.map(({ icon, label, href }, i) => (
+                  <NavLink
+                    key={i}
+                    href={`${siteConfig.pathname}${href}`}
+                    className="grid h-12 w-full grid-cols-[46px_1fr] items-center overflow-hidden whitespace-nowrap rounded border border-transparent capitalize outline-none ring-focus hover:bg-muted-3 focus-visible:ring-2 active:bg-muted-4 data-[active=true]:border-primary-8 data-[active=true]:bg-primary-3 data-[active=true]:text-primary-11"
+                  >
+                    <span className="mx-auto text-xl dynamic-icon">
+                      {icon}
+                    </span>
+                    <span>{label}</span>
+                  </NavLink>
+                ))}
+              </nav>
+            </motion.div>
+          </>,
+          document.body,
+        )}
     </header>
   );
 };
