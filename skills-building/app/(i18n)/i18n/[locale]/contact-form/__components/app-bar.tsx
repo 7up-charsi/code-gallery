3e759e28313c05c39f866718be0710616c3b3f5d
zeeshan <7up.charsi@gmail.com>
2024-08-15 10:30:58 +0500

    feat: locale switcher

--- skills-building/app/(i18n)/i18n/[locale]/contact-form/__components/app-bar.tsx
+++ skills-building/app/(i18n)/i18n/[locale]/contact-form/__components/app-bar.tsx
@@ -1,13 +1,9 @@
 'use client';
 
 import { PortfolioHeader } from '@/components/portfolio-header';
+import { LocaleSwitcher } from '@/components/locale-switcher';
 import { ThemeSwitcher } from '@/components/theme-switcher';
-import { useParams, useRouter } from 'next/navigation';
-import { Combobox } from '@typeweave/react/combobox';
-import { Skeleton } from '@typeweave/react/skeleton';
 import { Branding } from '@/components/branding';
-import { Input } from '@typeweave/react/input';
-import { LanguagesIcon } from 'lucide-react';
 import { siteConfig } from '../site.config';
 import React from 'react';
 
@@ -15,23 +11,8 @@ interface AppBarProps {}
 
 const displayName = 'AppBar';
 
-const options = [
-  { label: 'English', value: 'en-US' },
-  { label: 'Portuguese', value: 'pt-PT' },
-];
-
 export const AppBar = (props: AppBarProps) => {
   const {} = props;
-
-  const router = useRouter();
-
-  const { locale } = useParams<{ locale: string }>();
-
-  const [isMounted, setIsMounted] = React.useState(false);
-
-  React.useEffect(() => {
-    setIsMounted(true);
-  }, []);
 
   return (
     <header className="">
@@ -44,41 +25,12 @@ export const AppBar = (props: AppBarProps) => {
 
         <div className="grow"></div>
 
-        {!isMounted && (
-          <>
-            <Skeleton variant="rounded" className="h-9 w-40" />
-            <Skeleton variant="rounded" className="h-9 w-20" />
-          </>
-        )}
-
-        {isMounted && (
-          <>
-            <Combobox
-              value={options.find((opt) => opt.value === locale)}
-              options={options}
-              disableClearable
-              onChange={(value, reason) => {
-                if (reason === 'selectOption') {
-                  router.push(`/i18n/${value.value}/contact-form`);
-                }
-              }}
-              renderInput={(props) => (
-                <Input
-                  label="select locale"
-                  hideLabel
-                  {...props}
-                  classNames={{
-                    ...props.classNames,
-                    inputWrapper: 'h-9',
-                  }}
-                  startContent={<LanguagesIcon />}
-                  className="w-40"
-                />
-              )}
-            />
-          </>
-        )}
-
+        <LocaleSwitcher
+          locales={[
+            { label: 'English', value: 'en-US' },
+            { label: 'Portuguese', value: 'pt-PT' },
+          ]}
+        />
         <ThemeSwitcher />
       </div>
     </header>
---
+++ skills-building/components/locale-switcher.tsx
@@ -0,0 +1,91 @@
+import {
+  MenuArrow,
+  MenuContent,
+  MenuPortal,
+  MenuRadioGroup,
+  MenuRadioItem,
+  MenuRoot,
+  MenuTrigger,
+} from '@typeweave/react/menu';
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from 'next/navigation';
+import { useIsMounted } from '@typeweave/react/use-is-mounted';
+import { ChevronDownIcon, LanguagesIcon } from 'lucide-react';
+import { Skeleton } from '@typeweave/react/skeleton';
+import { Button } from '@typeweave/react/button';
+import React from 'react';
+
+interface LocaleSwitcherProps {
+  locales: { label: string; value: string }[];
+}
+
+const displayName = 'LocaleSwitcher';
+
+export const LocaleSwitcher = (props: LocaleSwitcherProps) => {
+  const { locales } = props;
+
+  const isMounted = useIsMounted();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { locale } = useParams<{ locale: string }>();
+
+  return (
+    <>
+      {!isMounted && (
+        <Skeleton variant="rounded" className="h-9 w-14" />
+      )}
+
+      {isMounted && (
+        <MenuRoot>
+          <MenuTrigger>
+            <Button
+              aria-label="theme switcher"
+              endContent={<ChevronDownIcon />}
+              className="w-14 gap-1"
+            >
+              <LanguagesIcon size={20} />
+            </Button>
+          </MenuTrigger>
+
+          <MenuPortal>
+            <MenuContent className="z-50">
+              <MenuArrow />
+
+              <MenuRadioGroup
+                value={locale}
+                label="themes"
+                hideLabel
+                onChange={(value) => {
+                  const newPathname = pathname.replace(locale, value);
+
+                  router.push(
+                    `${newPathname}?${searchParams.toString()}`,
+                  );
+                }}
+              >
+                {locales.map((ele) => (
+                  <MenuRadioItem
+                    key={ele.label}
+                    value={ele.value}
+                    classNames={{
+                      itemContent: ' capitalize',
+                    }}
+                  >
+                    {ele.label}
+                  </MenuRadioItem>
+                ))}
+              </MenuRadioGroup>
+            </MenuContent>
+          </MenuPortal>
+        </MenuRoot>
+      )}
+    </>
+  );
+};
+
+LocaleSwitcher.displayName = displayName;
