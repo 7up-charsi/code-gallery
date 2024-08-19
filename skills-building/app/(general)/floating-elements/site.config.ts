028464d92e5ffca2ec84d7fc990fffc17896cc8e
zeeshan <7up.charsi@gmail.com>
2024-08-19 12:37:20 +0500

    feat: floating elements base

---
+++ skills-building/app/(general)/floating-elements/site.config.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  pathname: '/floating-elements',
+  name: 'floating elements',
+  description:
+    'floating elements - Developed to demonstrate my expertise in web front-end development, featuring a floating element anchored beside its reference element, ensuring constant visibility and seamless collision avoidance.',
+};
--- skills-building/app/(general)/parallax/site.config.ts
+++ skills-building/app/(general)/parallax/site.config.ts
@@ -2,5 +2,5 @@ export const siteConfig = {
   pathname: '/parallax',
   name: 'parallax',
   description:
-    "parallax - Developed to demonstrate my expertise in web front-end development, featuring an advanced password checker that empowers users to assess their password's strength - whether it's strong, medium, or weak - and take control of their online security.",
+    'parallax - Developed to demonstrate my expertise in web front-end development, featuring a design that utilizes the parallax effect, where users experience captivating animations on scroll.',
 };
