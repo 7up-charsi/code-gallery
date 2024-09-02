1ed68bc79d23057379723e2a2c230dfc7ef85b22
zeeshan <7up.charsi@gmail.com>
2024-09-02 16:08:50 +0500

    fix: push notification site description

--- skillset-e2e/push-notification/config/site.ts
+++ skillset-e2e/push-notification/config/site.ts
@@ -1,4 +1,5 @@
 export const siteConfig = {
   name: 'push notification',
-  description: '',
+  description:
+    "Push notifications are messages that pop up on a user's mobile device or desktop from a website or application, even when the app is not open.",
 };
