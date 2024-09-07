a6d8edf8376f80ad89fafe672ad3105bed41ff2b
zeeshan <7up.charsi@gmail.com>
2024-09-07 08:16:00 +0500

    chore: push notifications config

--- skillset-e2e/push-notification/config/site.ts
+++ skillset-e2e/push-notification/config/site.ts
@@ -1,5 +1,5 @@
 export const siteConfig = {
   name: 'push notification',
   description:
-    "Push notifications are messages that pop up on a user's mobile device or desktop from a website or application, even when the app is not open.",
+    "Push notifications are messages that pop up on a user's mobile device or desktop from a website, even when the app is not open. They are typically used to re-engage users with an app or website, and can be used to send reminders, offers, news, or other updates.",
 };
