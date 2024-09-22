55c9fda114608f7c5feef8b4f9188486720c41ae
zeeshan <7up.charsi@gmail.com>
2024-09-22 19:13:48 +0500

    fix: config

--- front-end/config/root-site.ts
+++ front-end/config/root-site.ts
@@ -1,6 +1,6 @@
 export const rootSiteConfig = {
-  name: 'skills-building',
+  name: 'front-end',
   description:
-    'A collection of exercises to develop and improve my skills',
+    'A collection of exercises to develop and improve my front-end skills',
   category: 'front-end exercises',
 };
